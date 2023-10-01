b9dd04a4223e9bc01ccb124207641db2fe09097c
Shiva <kshiva.sdet@gmail.com>
2023-10-01 17:26:24 -0500

    TestNG Order Executed

--- BDD/src/test/java/TestNGLogic/DataProviderTest.java
+++ BDD/src/test/java/TestNGLogic/DataProviderTest.java
@@ -5,21 +5,23 @@ import org.testng.annotations.Test;
 
 public class DataProviderTest {
 	
-	@Test(dataProvider="getData")
-	public void testExecution(String name, String surname)
+	
+	@Test(dataProvider = "getData")
+	public void testMethod(String name, String surname)
 	{
 		System.out.println(name+" "+surname);
 	}
-
+	
+	
 	@DataProvider
 	public Object[][] getData()
 	{
 		return new Object[][]
 				{
-			{"Shivannarayana","Kundurthi"},
-			{"Bhulakshmi","Kundurthi"},
-			{"Jeshvick","Kundurthi"},
-			{"Vedhanshi","Kundurthi"}
+			{"Shiva","K"},
+			{"Bhulakshmi","K"},
+			{"Jeshvick","K"},
+			{"Vedhanshi","K"}
 				};
 	}
 }
